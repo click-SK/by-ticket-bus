ab5bd820a14943e6542af88da946bc9dee55ccea
click-SK <sashakushnir4991@gmail.com>
2023-07-28 22:24:34 +0300

    add curency

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Profile from "./components/profile/Profile";
 import AdminPanel from "./components/admin/AdminPanel";
 import AdminLogin from "./components/admin/AdminLogin";
 import ErrorComponent from "./components/Error/ErrorComponent";
+import BussСheme from "./components/booking/bus/BussСheme";
 import { useTheme } from "./hooks/use-thems";
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/edit" element={<AdminLogin />} />
         <Route path="/404" element={<ErrorComponent />} />
+        <Route path="/bus" element={<BussСheme/>} />
       </Routes>
       <Footer />
     </div>
